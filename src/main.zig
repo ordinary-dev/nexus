291511a35459310483ab7401dafe1e9fcc68f2f0
Ivan Reshetnikov <admin@ivanreshetnikov.ru>
2026-01-21 23:55:33 +0500

    Convert favicon to base64 at compile time

--- src/main.zig
+++ src/main.zig
@@ -19,9 +19,7 @@ pub fn main() !u8 {
     var html_file = try std.fs.cwd().createFile(parsedArgs.output_path, .{ .truncate = true });
     var html_buffer: [1024]u8 = undefined;
     var html_writer = html_file.writer(&html_buffer);
-    var writer_interface = &html_writer.interface;
-    try template.generateHTML(allocator, cfg, writer_interface);
-    try writer_interface.flush();
+    try template.generateHTML(cfg, &html_writer.interface);
 
     return 0;
 }
--- src/template.zig
+++ src/template.zig
@@ -46,18 +46,16 @@ const template_end =
     \\
 ;
 
+const favicon_format = "data:image/svg+xml;base64,{s}";
+
 const style = @embedFile("static/style.css");
 const script = @embedFile("static/script.js");
 const favicon = @embedFile("static/favicon.svg");
 
-pub fn generateHTML(allocator: std.mem.Allocator, cfg: config.Config, writer: *Writer) !void {
-    const b64 = std.base64.standard.Encoder;
-    const favicon_b64 = try allocator.alloc(u8, b64.calcSize(favicon.len));
-    defer allocator.free(favicon_b64);
-    _ = b64.encode(favicon_b64, favicon);
-
-    const favicon_href = try std.fmt.allocPrint(allocator, "data:image/svg+xml;base64,{s}", .{favicon_b64});
-    defer allocator.free(favicon_href);
+pub fn generateHTML(cfg: config.Config, writer: *Writer) !void {
+    const favicon_buf_size = comptime faviconHrefSize();
+    comptime var favicon_href: [favicon_buf_size]u8 = undefined;
+    comptime try faviconHref(&favicon_href);
 
     try writer.print(template_start, .{ cfg.title, style, favicon_href, script });
     for (cfg.groups) |group| {
@@ -70,4 +68,21 @@ pub fn generateHTML(allocator: std.mem.Allocator, cfg: config.Config, writer: *W
         try writer.print(group_end, .{});
     }
     _ = try writer.write(template_end);
+    try writer.flush();
+}
+
+fn faviconHrefSize() usize {
+    const b64 = std.base64.standard.Encoder;
+    return favicon_format.len - 3 + b64.calcSize(favicon.len);
+}
+
+fn faviconHref(buffer: []u8) !void {
+    @setEvalBranchQuota(10000);
+
+    const b64 = std.base64.standard.Encoder;
+    const b64_size = b64.calcSize(favicon.len);
+    var favicon_b64: [b64_size]u8 = undefined;
+    _ = b64.encode(&favicon_b64, favicon);
+
+    _ = try std.fmt.bufPrint(buffer, favicon_format, .{favicon_b64});
 }
